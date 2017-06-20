fd3ee8a7f86369914a9de1e2a7d9d3e9471fdba2
Taylor G <schoolemailtg328@gmail.com>
2017-06-20 15:37:34 -0400

    DataProcessor removeBlanks method works

--- src/com/Research/DataProcessor.java
+++ src/com/Research/DataProcessor.java
@@ -1,6 +1,6 @@
 package com.Research;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by Taylor on 6/11/2017.
@@ -8,16 +8,26 @@ import java.util.ArrayList;
 public class DataProcessor {
 
     //Removes blank ("") elements from ArrayList
-    public static ArrayList cleanUp(ArrayList arrayOfValues){
-        for (int i = 0; i < arrayOfValues.size(); i++){
-            if(arrayOfValues.get(i) == ""){
-                arrayOfValues.remove(i);
+    public static List removeBlanks(List tempArrayOfValues){
+        List arrayOfValues = new ArrayList<>();
+        for (int counter = 0; counter < tempArrayOfValues.size(); counter++){
+            arrayOfValues.add(tempArrayOfValues.get(counter));
+        }
+
+        while (arrayOfValues.contains("") || arrayOfValues.contains(null)){
+            arrayOfValues.remove("");
+            arrayOfValues.remove(null);
+        }
+        /*for (int i = 0; i < arrayOfValues.size(); i++){
+            if(arrayOfValues.get(i).equals("") || arrayOfValues.get(i) == null){
+                Object acceptor = arrayOfValues.remove(i);
             }
         }
+        return arrayOfValues; */
         return arrayOfValues;
     }
 
-    public boolean testDouble(String str) {
+    public boolean isDouble(String str) {
         try {
             Double.parseDouble(str);
             return true;
@@ -25,6 +35,8 @@ public class DataProcessor {
             return false;
         }
     }
+
+
 
 
 
--- src/com/Research/Main.java
+++ src/com/Research/Main.java
@@ -9,7 +9,8 @@ public class Main {
     public static void main(String[] args) {
         CSVReader cr = new CSVReader("C:/Users/Taylor/Desktop/joe.csv");
         String[] initialArray;
-        ArrayList<List<String>> finalValues = new ArrayList<>();
+        ArrayList<List> tempValues = new ArrayList<>();
+        ArrayList<List> finalValues = new ArrayList<>();
 
         //Read in values from csv, separated by "new"
         cr.setSplitString("new");
@@ -17,13 +18,26 @@ public class Main {
         //Get values from CSVReader
         initialArray = cr.getValues();
 
-        //Add values to finalValues
+        //Add values to tempValues
         for (int i = 0; i < initialArray.length; i++) {
-            List<String> parsedArray = Arrays.asList(initialArray[i].split("\\s*,\\s*"));
-            finalValues.add(parsedArray);
+            List parsedArray = Arrays.asList(initialArray[i].split("\\s*,\\s*"));
+            tempValues.add(parsedArray);
         }
 
         //TODO: Process data
+        for (int i = 0; i < tempValues.size(); i++){
+            finalValues.add(DataProcessor.removeBlanks(tempValues.get(i)));
+        }
+
+        //Print temp values
+        System.out.println("Temp Values ArrayList:\n" + tempValues);
+
+        //Example use:
+        //To print the 2nd array
+        System.out.println("\nThe 2nd temp array:\n" + tempValues.get(1));
+
+        //To print the fourth value of the second array
+        System.out.println("\nThe 4th value of the 2nd temp array:\n" + tempValues.get(1).get(3));
 
 
         //Print final values
